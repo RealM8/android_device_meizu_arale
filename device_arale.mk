e552957d0a421854480aa9a744f60215c3dcf279
TeamDevOne <chunkerstole@yandex.ru>
2017-12-24 22:24:19 +0200

    Initial commit

---
+++ device_arale.mk
@@ -0,0 +1,8 @@
+-include device/meizu/arale/device_common.mk
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_arale
+PRODUCT_DEVICE := arale
+
+
+# vim:set ai et ts=4 sw=4 sts=4 fenc=utf-8:
